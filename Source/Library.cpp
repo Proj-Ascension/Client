905e0e869beed7290ecacef54e9ffe15cc717f4c
Ben H <cmdr.phanphare@gmail.com>
2015-05-12 16:15:51 -0700

    Fix UX for running processes
    Give an error message before asking for a file selection (if
    applicable), and don’t style the filepicker/dialog boxes.

--- Source/Library.cpp
+++ Source/Library.cpp
@@ -25,20 +25,27 @@ Library::~Library()
 
 void Library::on_testLaunch_clicked()
 {
-    QFileDialog dialog(this);
-    dialog.setWindowTitle("Select Executable");
-    dialog.setFileMode(QFileDialog::ExistingFile);
 
-    if (dialog.exec()) {
-        QStringList files = dialog.selectedFiles();
-        QString file = files.at(0);
-#ifdef Q_WS_MACX
-        //Get the binary from the app bundle
-        QDir dir(file + "/Contents/MacOS");
-        QStringList fileList = dir.entryList();
-        file = dir.absoluteFilePath(fileList.at(2));//USUALLY this is the executable (after ., ..)
-#endif
-        runProcess(file);
+    if (!processRunning) {
+        QFileDialog dialog();
+        dialog.setWindowTitle("Select Executable");
+        dialog.setFileMode(QFileDialog::ExistingFile);
+
+        if (dialog.exec()) {
+            QStringList files = dialog.selectedFiles();
+            QString file = files.at(0);
+            #ifdef Q_WS_MACX
+                //Get the binary from the app bundle
+                QDir dir(file + "/Contents/MacOS");
+                QStringList fileList = dir.entryList();
+                file = dir.absoluteFilePath(fileList.at(2));//USUALLY this is the executable (after ., ..)
+            #endif
+            runProcess(file);
+        }
+    } else {
+        QMessageBox messageBox();
+        messageBox.setText("Error: an application is already running.");
+        messageBox.exec();
     }
 }
 
@@ -48,10 +55,6 @@ void Library::runProcess(QString location) {
         runningProcess->start(location);
         runningProcess->waitForStarted();
         processRunning = true;
-    } else {
-        QMessageBox messageBox(this);
-        messageBox.setText("Error: an application is already running.");
-        messageBox.exec();
     }
 }
 
