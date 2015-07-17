e85d8c5b1d8f06c0ed0ef46c5540bda8d112ded7
Ellis kenyo <elken@mykolab.com>
2015-07-17 12:16:29 +0100

    Find origin games, refactor the find a bit

--- Source/Library.h
+++ Source/Library.h
@@ -4,37 +4,40 @@
 
 #include <QWidget>
 #include <QProcess>
+#include <QDir>
 
 namespace Ui
 {
-    class Library;
+	class Library;
 }
 
 class Library : public QWidget
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    Library(Database db);
-    ~Library();
+	Library(Database db);
+	~Library();
 
-private slots:
-    void on_testLaunch_clicked();
-    void on_addGame_clicked();
-    void finished(int exitCode, QProcess::ExitStatus exitStatus);
-    void onLaunchError(QProcess::ProcessError error);
+	private slots:
+	void on_testLaunch_clicked();
+	void on_addGame_clicked();
+	void finished(int exitCode, QProcess::ExitStatus exitStatus);
+	void onLaunchError(QProcess::ProcessError error);
 
-    void on_removeGame_clicked();
+	void on_removeGame_clicked();
 
 private:
-    Database db;
-    Ui::Library* ui;
-    QProcess* runningProcess;
-    QList<QString> steamDirectoryList;
+	Database db;
+	Ui::Library* ui;
+	QProcess* runningProcess;
+	QList<QString> steamDirectoryList;
 
-    bool isProcessRunning() const;
-    void runProcess(QString file, QString workingDirectory);
-    void refreshGames();
-    void findSteamGames();
-    void parseAcf();
+	bool isProcessRunning() const;
+	QStringList recursiveFindFiles(QDir dir, QStringList ignoreList);
+	void runProcess(QString file, QString workingDirectory);
+	void refreshGames();
+	void findSteamGames(QDir steamRoot);
+	void findOriginGames(QDir originRoot);
+	void parseAcf();
 };
