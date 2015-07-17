905e663d06a5c1649d9fe1b44405aba6eeea6c4c
Jake Schadewe <hal9007.mc@gmail.com>
2015-07-16 23:40:39 -0500

    Clean up entry point, add general initialization header

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,121 +1,13 @@
-#include <QMessageBox>
-#include <QFontDatabase>
-#include <QFile>
-#include <QSize>
+#include "Init.h"
+
 #include <QApplication>
-#include <QSettings>
+#include <QFile>
 
 #ifdef Q_OS_WIN
 #include "BorderlessWindow.h"
 #else
 #include "UnixWindow.h"
 #endif
-
-void g_initSettings()
-{
-    QString configFile = "config.ini", paletteFile = "palette.ini";
-    QSettings config(configFile, QSettings::IniFormat), palette(paletteFile, QSettings::IniFormat);
-
-    if (!QFile(configFile).exists() && config.isWritable())
-    {
-        // TODO: Set default config settings.
-    }
-    if (!QFile(paletteFile).exists() && palette.isWritable())
-    {
-        palette.beginGroup("Primary");
-
-        palette.setValue("ActiveElement"  , "#FFFFFF");
-        palette.setValue("ActiveSelection", "#242424");
-        palette.setValue("HoverSelection" , "#1B1B1B");
-        palette.setValue("LightText"      , "#FFFFFF");
-        palette.setValue("DarkText"       , "#242424");
-        palette.setValue("SubText"        , "#8B8B8B");
-        palette.setValue("PrimaryBase"    , "#282828");
-        palette.setValue("SecondaryBase"  , "#1F1F1F");
-        palette.setValue("TertiaryBase"   , "#181818");
-        palette.setValue("DarkestBase"    , "#0F0F0F");
-        palette.endGroup();
-
-        palette.beginGroup("Accent");
-
-        palette.setValue("LightAccent" , "#68AD28");
-        palette.setValue("MediumAccent", "#589828");
-        palette.setValue("DarkAccent"  , "#487D28");
-        palette.endGroup();
-    }
-}
-
-void g_loadFonts(QApplication* application)
-{
-    // Font
-    QFont mainFont = application->font();
-    mainFont.setStyleStrategy(QFont::PreferAntialias);
-    application->setFont(mainFont);
-
-    // Dynamically load fonts
-    QStringList list;
-    list << "Montserrat-Black.ttf" <<
-        "Montserrat-Bold.ttf" <<
-        "Montserrat-Light.ttf" <<
-        "Montserrat-Regular.ttf" <<
-        "Montserrat-Thin.ttf" <<
-        "Roboto-BlackItalic.ttf" <<
-        "Roboto-Black.ttf" <<
-        "Roboto-BoldItalic.ttf" <<
-        "Roboto-Bold.ttf" <<
-        "Roboto-Italic.ttf" <<
-        "Roboto-LightItalic.ttf" <<
-        "Roboto-Light.ttf" <<
-        "Roboto-MediumItalic.ttf" <<
-        "Roboto-Medium.ttf" <<
-        "Roboto-Regular.ttf" <<
-        "Roboto-ThinItalic.ttf" <<
-        "Roboto-Thin.ttf" <<
-        "SourceCodePro-Black.ttf" <<
-        "SourceCodePro-Bold.ttf" <<
-        "SourceCodePro-ExtraLight.ttf" <<
-        "SourceCodePro-Light.ttf" <<
-        "SourceCodePro-Medium.ttf" <<
-        "SourceCodePro-Regular.ttf" <<
-        "SourceCodePro-Semibold.ttf" <<
-        "SourceSansPro-BlackItalic.ttf" <<
-        "SourceSansPro-Black.ttf" <<
-        "SourceSansPro-BoldItalic.ttf" <<
-        "SourceSansPro-Bold.ttf" <<
-        "SourceSansPro-ExtraLightItalic.ttf" <<
-        "SourceSansPro-ExtraLight.ttf" <<
-        "SourceSansPro-Italic.ttf" <<
-        "SourceSansPro-LightItalic.ttf" <<
-        "SourceSansPro-Light.ttf" <<
-        "SourceSansPro-Regular.ttf" <<
-        "SourceSansPro-SemiboldItalic.ttf" <<
-        "SourceSansPro-Semibold.ttf";
-
-    int fontID(-1);
-    bool fontWarningShown(false);
-    for (auto font : list)
-    {
-        QFile res(":/Typeface/" + font);
-        if (!res.open(QIODevice::ReadOnly))
-        {
-            if (!fontWarningShown)
-            {
-                QMessageBox::warning(0, "Application", (QString) "Warning: Unable to load font " + QChar(0x00AB) + font + QChar(0x00BB) + ".");
-                fontWarningShown = true;
-            }
-        }
-        else
-        {
-            fontID = QFontDatabase::addApplicationFontFromData(res.readAll());
-            if (fontID == -1 && !fontWarningShown)
-            {
-                QMessageBox::warning(0, "Application", (QString) "Warning: Unable to load font " + QChar(0x00AB) + font + QChar(0x00BB) + ".");
-                fontWarningShown = true;
-            }
-        }
-    }
-
-}
 
 int main(int argc, char* argv[])
 {
@@ -135,8 +27,8 @@ int main(int argc, char* argv[])
         application->setStyleSheet(styleSheet);
     }
 
-    g_initSettings();
-    g_loadFonts(application);
+    entryPoint::initSettings();
+    entryPoint::initFonts(application);
 
     #ifdef Q_OS_WIN
         // Background color
