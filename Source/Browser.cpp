c32b0458002fda627c7684e82227d1f8e057ebaf
Ellis Kenyo <elken.tdos@gmail.com>
2015-08-02 16:35:09 +0100

    Added comments for Browser

--- Source/Browser.cpp
+++ Source/Browser.cpp
@@ -1,6 +1,11 @@
 #include "Browser.h"
 #include "ui_Browser.h"
 
+//! Constructor for the browser object.
+/*!
+ * Creates the Browser object, allocates the UI and sets up a parent QWidget
+ * \param parent QWidget* object to use as the parent.
+*/
 Browser::Browser(QWidget* parent) :
     QWidget(parent),
     ui(new Ui::Browser)
@@ -9,6 +14,10 @@ Browser::Browser(QWidget* parent) :
     this->setObjectName("browserUI");
 }
 
+//! Destructor for the Browser object.
+/*!
+ * Unload the UI
+*/
 Browser::~Browser()
 {
     delete ui;
--- Source/Browser.h
+++ Source/Browser.h
@@ -2,14 +2,22 @@
 
 #include <QWidget>
 
+
+/* Namespace containing the Browser UI */
 namespace Ui
 {
 class Browser;
 }
 
+//! WebKit Browser class
+/*!
+ * A simple QWebKit browser of the sub-reddit (Soon to be deprecated).
+ *
+ * Inherits: [QWidget](http://doc.qt.io/qt-5/qwidget.html)
+*/
 class Browser : public QWidget
 {
-    Q_OBJECT
+    Q_OBJECT 
 
 public:
     explicit Browser(QWidget* parent = 0);
