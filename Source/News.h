7577025bb38891957ef7551206ffb483cce899bd
Benjamin Cheng <ben@bcheng.cf>
2015-10-20 17:33:04 -0400

    News feed threading working with multiple loading at once
    The QNetworkReply object was a member field.

--- Source/News.h
+++ Source/News.h
@@ -11,27 +11,23 @@
 */
 class News : public QWidget
 {
-
     Q_OBJECT
-private:
-    int connectionId;
-    QXmlStreamReader xml;
-    QNetworkAccessManager manager;
-    QNetworkReply* reply;
-    QListWidget* RSSList;
-    QLabel* RSSListLabel;
-    QLineEdit* rssAddress;
-    QHBoxLayout* horLayout = new QHBoxLayout();
-    void getRSSFeed(QString url);
-    void saveFeeds(QString title, QString url);
-    void loadFeeds();
-    QSettings* rss;
 
 public slots:
     void setRSSFeed();
-    void onRSSReturned();
+    void onRSSReturned(QNetworkReply* reply);
 
 public:
     explicit News(QSettings* p, QWidget* parent = 0);
     ~News();
+
+private:
+    QHBoxLayout* hNewsLayout;
+    QLineEdit* rssAddress;
+    QNetworkAccessManager* manager;
+    QSettings* rss;
+
+    void getRSSFeed(QString url);
+    void saveFeeds(QString title, QString url);
+    void loadFeeds();
 };
