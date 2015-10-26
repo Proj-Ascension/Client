1987a7006da70597347f81d5aa5e0327ce1801df
Jake Schadewe <hal9007.mc@gmail.com>
2015-10-26 19:34:47 -0400

    Mass refactor; Qt formatting style and source organization

---
+++ src/init.h
@@ -0,0 +1,132 @@
+#pragma once
+
+#include <QApplication>
+#include <QFontDatabase>
+#include <QSettings>
+#include <QMessageBox>
+#include <QFile>
+#include <QDir>
+#include <QStandardPaths>
+#include <QDebug>
+
+/** Namespace for entry point initialization functions. */
+namespace entryPoint
+{
+void initSettings(QApplication &application)
+{
+
+   /* QSettings config(QSettings::IniFormat, QSettings::UserScope, "Project Ascension", "config");
+    QSettings rss(QSettings::IniFormat, QSettings::UserScope, "Project Ascension", "rss");
+    QSettings palette(QSettings::IniFormat, QSettings::UserScope, "Project Ascension", "palette");*/
+
+    QSettings config(QString("config.ini"), QSettings::IniFormat);
+    QSettings rss(QString("rss.ini"), QSettings::IniFormat);
+    QSettings palette(QString("palette.ini"), QSettings::IniFormat);
+
+    if (!QFile("config.ini").exists() && config.isWritable())
+    {
+        // TODO: Set default config settings.
+    }
+
+    if (!QFile("rss.ini").exists() && rss.isWritable())
+    {
+        // TODO: Set default rss
+    }
+
+    if (!QFile(palette.fileName()).exists() && palette.isWritable())
+    {
+        palette.beginGroup("Primary");
+
+        palette.setValue("ActiveElement", "#FFFFFF");
+        palette.setValue("InactiveSelection", "#686868");
+        palette.setValue("HoverSelection", "#ADADAD");
+        palette.setValue("DarkElement", "#4D5250");
+        palette.setValue("LightText", "#FFFFFF");
+        palette.setValue("DarkText", "#242424");
+        palette.setValue("SubText", "#B2B4B3");
+        palette.setValue("PrimaryBase", "#282828");
+        palette.setValue("SecondaryBase", "#1F1F1F");
+        palette.setValue("TertiaryBase", "#131313");
+        palette.setValue("DarkestBase", "#0F0F0F");
+        palette.endGroup();
+
+        palette.beginGroup("Accent");
+
+        palette.setValue("LightAccent", "#E58F12");
+        palette.setValue("MediumAccent", "#895f06");
+        palette.setValue("DarkAccent", "#6a4a05");
+        palette.endGroup();
+    }
+}
+
+void initFonts(QApplication &application)
+{
+    // Font
+    QFont mainFont = application.font();
+    mainFont.setStyleStrategy(QFont::PreferAntialias);
+    application.setFont(mainFont);
+
+    // Dynamically load fonts
+    QStringList list;
+    list << "Montserrat-Black.ttf" <<
+        "Montserrat-Bold.ttf" <<
+        "Montserrat-Light.ttf" <<
+        "Montserrat-Regular.ttf" <<
+        "Montserrat-Thin.ttf" <<
+        "Roboto-BlackItalic.ttf" <<
+        "Roboto-Black.ttf" <<
+        "Roboto-BoldItalic.ttf" <<
+        "Roboto-Bold.ttf" <<
+        "Roboto-Italic.ttf" <<
+        "Roboto-LightItalic.ttf" <<
+        "Roboto-Light.ttf" <<
+        "Roboto-MediumItalic.ttf" <<
+        "Roboto-Medium.ttf" <<
+        "Roboto-Regular.ttf" <<
+        "Roboto-ThinItalic.ttf" <<
+        "Roboto-Thin.ttf" <<
+        "SourceCodePro-Black.ttf" <<
+        "SourceCodePro-Bold.ttf" <<
+        "SourceCodePro-ExtraLight.ttf" <<
+        "SourceCodePro-Light.ttf" <<
+        "SourceCodePro-Medium.ttf" <<
+        "SourceCodePro-Regular.ttf" <<
+        "SourceCodePro-Semibold.ttf" <<
+        "SourceSansPro-BlackItalic.ttf" <<
+        "SourceSansPro-Black.ttf" <<
+        "SourceSansPro-BoldItalic.ttf" <<
+        "SourceSansPro-Bold.ttf" <<
+        "SourceSansPro-ExtraLightItalic.ttf" <<
+        "SourceSansPro-ExtraLight.ttf" <<
+        "SourceSansPro-Italic.ttf" <<
+        "SourceSansPro-LightItalic.ttf" <<
+        "SourceSansPro-Light.ttf" <<
+        "SourceSansPro-Regular.ttf" <<
+        "SourceSansPro-SemiboldItalic.ttf" <<
+        "SourceSansPro-Semibold.ttf";
+
+    int fontID(-1);
+    bool fontWarningShown(false);
+    for (auto font : list)
+    {
+        QFile res(":/typeface/" + font);
+        if (!res.open(QIODevice::ReadOnly))
+        {
+            if (!fontWarningShown)
+            {
+                QMessageBox::warning(0, "Application", (QString) "Warning: Unable to load font " + QChar(0x00AB) + font + QChar(0x00BB) + ".");
+                fontWarningShown = true;
+            }
+        }
+        else
+        {
+            fontID = QFontDatabase::addApplicationFontFromData(res.readAll());
+            if (fontID == -1 && !fontWarningShown)
+            {
+                QMessageBox::warning(0, "Application", (QString) "Warning: Unable to load font " + QChar(0x00AB) + font + QChar(0x00BB) + ".");
+                fontWarningShown = true;
+            }
+        }
+    }
+}
+}
---
+++ src/main.cpp
@@ -0,0 +1,64 @@
+#include "init.h"
+#include "gui/wizards/drm_setup_wizard.h"
+
+#include <QApplication>
+#include <QFile>
+#include <QDebug>
+#include <iostream>
+#include <QIcon>
+
+
+
+#ifdef Q_OS_WIN
+#include "gui/borderless/borderless_window.h"
+
+#else
+#include "gui/unix_window.h"
+
+#endif
+
+int main(int argc, char* argv[])
+{
+    QApplication::setStyle("fusion");
+    QApplication* application = new QApplication(argc, argv);
+	bool dbExists = QFile("ascension.db").exists();
+    DRMSetupWizard* wiz = new DRMSetupWizard();
+
+    #ifndef Q_OS_WIN
+        // dynamic loading of the icon under Linux/UNIX
+        application->setWindowIcon(QIcon(":/system_menu/icons/ascension_icon.ico"));
+    #endif
+
+    // Global stylesheet
+    QFile stylesheet(":/styles/pa_client.css");
+    if (stylesheet.open(QFile::ReadOnly))
+    {
+        QString styleSheet = stylesheet.readAll();
+        application->setStyleSheet(styleSheet);
+    }
+
+    entryPoint::initSettings(*application);
+    entryPoint::initFonts(*application);
+
+    #ifdef Q_OS_WIN
+        // Background color
+        // This is only for WinApi window, Qt widgets use BorderlessWindow.css stylesheet
+        HBRUSH windowBackground = CreateSolidBrush(RGB(15, 15, 15));
+
+        // Create a Win window
+        BorderlessWindow window(application, windowBackground, 1152, 648);
+        window.setMinimumSize(830, 550);
+    #else
+        // Create a Unix window
+        UnixWindow window;
+        window.setMinimumSize(830, 550);
+    #endif
+
+    // Launch
+	if (!dbExists)
+	{
+		wiz->show();
+	}
+	application->exec();
+    return 0;
+}
